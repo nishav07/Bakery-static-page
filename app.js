a0d61f0c46093cb970ee2ab7c4a7aa0849dd2db5
unknown <nishavkumar07@gmail.com>
2025-07-27 20:46:25 +0530

    added js

--- app.js
+++ app.js
@@ -1,3 +1,37 @@
+let togglebtn = document.getElementById("toggle");
+let html = document.documentElement;
+togglebtn.addEventListener("click",() => {
+    togglebtn.style.opacity = 0;
+
+    setTimeout(() => {
+        toggle(togglebtn);
+        togglebtn.style.opacity = 1;
+    },300)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// toggle krne wala funx
+
 function toggle(el){
-    el.classlist.contain("sun").toggle("moon")
+    if(el.classList.contains("fa-moon")){
+        el.classList.remove("fa-moon")
+        el.classList.add("fa-sun")
+        html.setAttribute("data-theme" , "dark")
+
+    } else {
+        el.classList.add("fa-moon")
+        html.setAttribute("data-theme" , "light")
+    }
 }
